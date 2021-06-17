6ebeb6692e48a152a1768cee305924cdfd3138b4
SpaceOcean <komsomolenko11@gmail.com>
2021-06-17 21:40:36 +0300

    test commit

--- Project_D_06_21/IngredientsViewController.swift
+++ Project_D_06_21/IngredientsViewController.swift
@@ -11,29 +11,17 @@ class IngredientsViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print("dvdv")
+        print("test")
         return 10
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "cdcdcdc"
+        cell.textLabel?.text = "textLabel"
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
